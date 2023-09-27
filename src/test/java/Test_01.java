593c3d4317b8e84e7e4b176023562106f047b0be
Manasa <manasa131326@gmail.com>
2023-09-27 18:10:02 +0530

    3rd commit

--- src/test/java/Test_01.java
+++ src/test/java/Test_01.java
@@ -4,6 +4,7 @@ public class Test_01 {
 	public static void main(String[] args) {
 		System.out.println("Hi");
 		System.out.println("Task is completed");
+		System.out.println("Task 1 is completed");
 
 	}
 
